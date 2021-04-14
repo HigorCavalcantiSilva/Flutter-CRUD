e55f06591f88450810d30a4a4b843c2c3d1b333f
Higor Cavalcanti Silva <higorsilva07.hs@gmail.com>
2021-04-14 14:29:36 -0400

    First Commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Higor',
+    email: 'teste1@teste.com',
+    avatarUrl: 'https://png.pngtree.com/png-clipart/20190705/original/pngtree-man-avatar-icon-professional-man-character-png-image_4356027.jpg'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Hicker',
+    email: 'teste2@teste.com',
+    avatarUrl: 'https://png.pngtree.com/png-clipart/20200225/original/pngtree-businessman-avatar-icon-vector-download-vector-user-icon-avatar-silhouette-social-png-image_5257566.jpg'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Hacker',
+    email: 'teste3@teste.com',
+    avatarUrl: 'https://png.pngtree.com/png-clipart/20190922/original/pngtree-business-male-user-avatar-vector-png-image_4774078.jpg'
+  )
+};
